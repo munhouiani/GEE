4c97327a1dee1313ff4ac52775d30c3dcb41ca9a
Mun Hou <mhwong2007@gmail.com>
2020-07-06 17:20:36 +0800

    Add mean related features * mean_duration * mean_packet * mean_num_fo_bytes * mean_packet_rate * mean_byte_rate

--- feature_extraction.py
+++ feature_extraction.py
@@ -22,8 +22,27 @@ class FeatureExtractor:
         # patch time window
         self.df = patch_time_windows(df=df, window_seconds=window_seconds)
 
+        # extract packet rate
+        self.df = (
+            self.df
+                .withColumn('packet_rate', col('packet') / col('duration'))
+        )
+
+        # extract packet rate
+        self.df = (
+            self.df
+                .withColumn('packet_rate', col('packet') / col('duration'))
+        )
+
+        # extract byte rate
+        self.df = (
+            self.df
+                .withColumn('byte_rate', col('num_of_bytes') / col('duration'))
+        )
+
         # udf functions of extraction methods
         self.extract_num_flow_udf = pandas_udf(self.extract_num_flow, 'double')
+        self.mean_udf = pandas_udf(self.mean, 'double')
 
     @staticmethod
     def extract_num_flow(grouped_data: pd.Series) -> float:
@@ -37,6 +56,18 @@ class FeatureExtractor:
 
         return float(len(grouped_data))
 
+    @staticmethod
+    def mean(grouped_data: pd.Series) -> float:
+        """
+        Extract mean of a given pandas Series
+        :param grouped_data: grouped data
+        :type grouped_data: pd.Series
+        :return: mean value
+        :rtype: float
+        """
+
+        return grouped_data.mean()
+
     def extract_features(self) -> pyspark.sql.dataframe:
         df = (
             self.df
@@ -44,7 +75,12 @@ class FeatureExtractor:
                 .groupby('time_window', 'src_ip')
                 # start extracting feature
                 .agg(
-                self.extract_num_flow_udf(lit(1)).alias('num_flow')
+                self.extract_num_flow_udf(lit(1)).alias('num_flow'),
+                self.mean_udf('duration').alias('mean_duration'),
+                self.mean_udf('packet').alias('mean_packet'),
+                self.mean_udf('num_of_bytes').alias('mean_num_of_bytes'),
+                self.mean_udf('packet_rate').alias('mean_packet_rate'),
+                self.mean_udf('byte_rate').alias('mean_byte_rate'),
             )
                 # filter out num_flow < 10
                 .filter((col('num_flow') >= 10))
@@ -52,6 +88,8 @@ class FeatureExtractor:
                 .orderBy('time_window', 'src_ip')
                 # drop num_flow
                 .drop('num_flow')
+                # fill na
+                .na.fill(0.0)
         )
 
         return df
